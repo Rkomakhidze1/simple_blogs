980a0f0d0a8b6e37a69bb4f0a0abaff0243f6c1f
rezoKom <rezokomakhidze1@gmail.com>
2021-01-13 21:16:53 +0400

    commented out header test file

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,33 +1,33 @@
-const Page = require("./helpers/page");
+// const Page = require("./helpers/page");
 
-let page;
-beforeEach(async () => {
-  page = await Page.build();
-  await page.goto("http://localhost:3000/");
-});
+// let page;
+// beforeEach(async () => {
+//   page = await Page.build();
+//   await page.goto("http://localhost:3000/");
+// });
 
-afterEach(async () => {
-  await page.close();
-});
+// afterEach(async () => {
+//   await page.close();
+// });
 
-test("header shows correct text", async () => {
-  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+// test("header shows correct text", async () => {
+//   const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
 
-  expect(text).toEqual("Blogster");
-});
+//   expect(text).toEqual("Blogster");
+// });
 
-test("clicking login should redirect to google oauth", async () => {
-  await page.click("ul a");
+// test("clicking login should redirect to google oauth", async () => {
+//   await page.click("ul a");
 
-  const url = await page.url();
+//   const url = await page.url();
 
-  expect(url).toMatch(/accounts\.google\.com/);
-});
+//   expect(url).toMatch(/accounts\.google\.com/);
+// });
 
-test("logout button should be displayed when logged in", async () => {
-  await page.login();
-  //   await page.waitFor("ul li:last-child a");
-  const text = await page.$eval("ul li:last-child a", (el) => el.innerHTML);
+// test("logout button should be displayed when logged in", async () => {
+//   await page.login();
+//   //   await page.waitFor("ul li:last-child a");
+//   const text = await page.$eval("ul li:last-child a", (el) => el.innerHTML);
 
-  expect(text).toEqual("Logout");
-});
+//   expect(text).toEqual("Logout");
+// });
